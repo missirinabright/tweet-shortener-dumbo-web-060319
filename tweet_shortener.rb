8730ceaf68bef1b754d447af924f491d789e690c
Irina Bright <missirinabright@gmail.com>
2019-06-02 20:43:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -8,20 +8,20 @@ tweets = ["Hey guys, can anyone teach me how to be cool? I really want to be the
 "New game. Middle aged tweet followed by #youngPeopleHashTag Example: Gotta get my colonoscopy and mammogram soon. Prevention is key! #swag"]
 
 
-def word_substituter(tweet)
-  tweet = {
-  "hello" => "hi",
-  " to " => " 2  ",
-  " two " => " 2 ",
-  " too " => " 2 ",
-  " for " => " 4 ",
-  " four " => " 4 ",
-  " be " => " b ",
-  " you " => " u ",
-  " at " => " u ",
-  " and " => " @ "
-  }
-end
+# def word_substituter(tweet)
+#   tweet = {
+#   "hello" => "hi",
+#   " to " => " 2  ",
+#   " two " => " 2 ",
+#   " too " => " 2 ",
+#   " for " => " 4 ",
+#   " four " => " 4 ",
+#   " be " => " b ",
+#   " you " => " u ",
+#   " at " => " u ",
+#   " and " => " @ "
+#   }
+# end
 
       
 def word_substituter(tweet)
