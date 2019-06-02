17b9633a7a82d0a1f186d3ef4872f3c230ccf658
Irina Bright <missirinabright@gmail.com>
2019-06-02 20:54:06 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,56 @@
-# Write your code here.
+tweets = ["Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!", 
+"OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?",
+
+"I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real.",
+
+"GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!",
+
+"New game. Middle aged tweet followed by #youngPeopleHashTag Example: Gotta get my colonoscopy and mammogram soon. Prevention is key! #swag"]
+
+
+# def word_substituter(tweet) (alternative attempt)
+#   tweet = {
+#   "hello" => "hi",
+#   " to " => " 2  ",
+#   " two " => " 2 ",
+#   " too " => " 2 ",
+#   " for " => " 4 ",
+#   " four " => " 4 ",
+#   " be " => " b ",
+#   " you " => " u ",
+#   " at " => " u ",
+#   " and " => " @ "
+#   }
+# end
+
+      
+def word_substituter(tweet)
+  #added spaces before and after each element, to prevent the replacements inside of a bigger word containing these elements
+  
+  tweet = tweet.gsub("hello", " hi ").gsub(" too ", " 2 ").gsub(" two ", " 2 ").gsub(" to ", " 2 ").gsub(" be ", " b ").gsub(" you ", " u ").gsub(" at ", " @ ").gsub(" and ", " & ").gsub(" For ", " 4 ").gsub(" for ", " 4 ")
+  tweet
+end
+
+def bulk_tweet_shortener(tweet)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  next_tweet = selective_tweet_shortener(tweet)
+  if next_tweet.length > 140
+    next_tweet = next_tweet[0..139]
+  else
+    next_tweet = next_tweet
+  end
+  next_tweet
+end 
